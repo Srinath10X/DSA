27cad3066ac393a6e739b8e5fb1e9282cc458666
Srinath10X <srinathcreates@gmail.com>
2025-07-11 00:39:26 +0530

    algo(bubble-sort): added bubble-sort algorithm

---
+++ algorithms/sorting/bubble-sort.cpp
@@ -0,0 +1,35 @@
+#include <iterator>
+
+void swap(int *a, int *b) {
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+int main() {
+  int arr[] = {5, 1, 4, 2};
+
+  /**
+   * Bubble Sort: Continually compare adjacent elements and swap them
+   *
+   * if ascending order: swap the largest to end
+   * if descending order: swap the smallest to end
+   *
+   * I1: [5, 1, 4, 2] -> [1, 4, 2, 5]
+   *
+   * we can observe that in first itteration we swap the largest element to end
+   * and in every next itteration we swap the next largest element to end. 🤯
+   *
+   * and in the second loop we are reducing the no of unwanted itterations cause
+   * we know that the largest element is already at the end
+   */
+  for (size_t i = 0; i < std::size(arr) - 1; ++i) {
+    for (size_t j = 0; j < std::size(arr) - i - 1; ++j) {
+      if (arr[j] > arr[j + 1]) {
+        swap(&arr[j], &arr[j + 1]);
+      }
+    }
+  }
+
+  return 0;
+}
