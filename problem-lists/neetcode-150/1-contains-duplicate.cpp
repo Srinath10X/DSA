69479da23653967453ded2f1ac5173221421b879
Srinath10X <srinathcreates@gmail.com>
2025-07-15 00:07:08 +0530

    nc(#1): add contains duplicate solution

---
+++ problem-lists/neetcode-150/1-contains-duplicate.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  bool containsDuplicate(vector<int> &nums) {
+    unordered_set<int> uset;
+
+    for (int num : nums) {
+      if (uset.find(num) != uset.end())
+        return true;
+      else
+        uset.insert(num);
+    }
+
+    return false;
+  }
+};
