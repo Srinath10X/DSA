1b4df2805af68d955166f61b14fb5ab771a46bff
Srinath10X <srinathcreates@gmail.com>
2025-07-26 15:14:47 +0530

    chore: add optimal solution to second largest element

--- gfg160/day1/optimal.cpp
+++ gfg160/day1/optimal.cpp
@@ -1,10 +1,9 @@
 #include <bits/stdc++.h>
 
 /**
- * Better solution
- * 1. Traverse the array and find the largest element
- * 2. Traverse the array and find the maximum element which is
- *    not equal to the largest element.
+ * Optimal solution
+ * Traverse the array and assign the largest and second largest
+ * while traversing
  *
  *   Time Complexity: O(n)
  *   Space Complexity: O(1) as I'm passing vector by reference
@@ -13,11 +12,12 @@ int getSecondLargest(std::vector<int> &arr) {
   int largest = -1, second_largest = -1;
 
   for (int i = 0; i < arr.size(); ++i) {
-    if (arr[i] > largest) largest = arr[i];
-  }
-
-  for (int i = 0; i < arr.size(); ++i) {
-    if (arr[i] > second_largest && arr[i] != largest) second_largest = arr[i];
+    if (arr[i] > largest) {
+      second_largest = largest;
+      largest = arr[i];
+    } else if (arr[i] > second_largest && arr[i] < largest) {
+      second_largest = arr[i];
+    }
   }
 
   return second_largest;
