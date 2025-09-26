fee20fbde97fd80a1a7dce42fc582b33cbe9d3bd
Srinath10X <srinathcreates@gmail.com>
2025-09-26 20:20:53 +0530

    chore: add insertion sort recursive version

--- algorithms/insertion_sort.c
+++ algorithms/insertion_sort.c
@@ -2,6 +2,12 @@
  * Algorithm: Insertion sort
  * Time Complexity: O(n^2), O(n^2), O(n)
  */
+void swap(int *a, int *b) {
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
 void insertion_sort(int arr[], int n) {
   for (int i = 1; i < n; ++i) {
     int j = i - 1;
@@ -13,4 +19,18 @@ void insertion_sort(int arr[], int n) {
 
     arr[j + 1] = key;
   }
+}
+
+void insertion_sort_recursive(int arr[], int i, int n) {
+  if (i == n) {
+    return;
+  }
+
+  int j = i;
+
+  while (j >= 0 && arr[j - 1] > arr[j]) {
+    swap(&arr[j], &arr[j - 1]), --j;
+  }
+
+  insertion_sort_recursive(arr, i + 1, n);
 }
